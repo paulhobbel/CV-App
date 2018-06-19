37e33c12a4ff1b144acd6a195c839e081e7bb314
Dizzy <itsdizzy40@gmail.com>
2018-06-19 02:41:53 +0200

    Messing around with the ActionBarDrawerToggle

--- app/src/main/java/me/paulhobbel/cvapp/activities/MainActivity.java
+++ app/src/main/java/me/paulhobbel/cvapp/activities/MainActivity.java
@@ -6,11 +6,13 @@
 package me.paulhobbel.cvapp.activities;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -34,6 +36,8 @@ public class MainActivity extends AppCompatActivity implements NavigationActivit
     DrawerLayout drawerLayout;
     @BindView(R.id.nav_view)
     NavigationView navigationView;
+
+    ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,12 +70,20 @@ public class MainActivity extends AppCompatActivity implements NavigationActivit
     }
 
     @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 drawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
+
+        drawerToggle.syncState();
 
         return super.onOptionsItemSelected(item);
     }
@@ -118,5 +130,22 @@ public class MainActivity extends AppCompatActivity implements NavigationActivit
     @Override
     public void onNavigationFragmentAttach(NavigationFragment fragment) {
         navigationView.setCheckedItem(fragment.getNavigationId());
+
+        drawerLayout.removeDrawerListener(drawerToggle);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+
+            public void onDrawerOpened(View view) {
+                super.onDrawerOpened(view);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+        };
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(drawerToggle);
     }
 }
